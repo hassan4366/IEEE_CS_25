bf9256f32f592dd8b9b5b77b82394b94e7f08447
Hassan Hammam <hassanibrahimhammam@gmail.com>
2025-03-13 07:30:48 +0200

    Add files via upload

---
+++ Front_End/J_S/task10/task11/assin27to30/main1.js
@@ -0,0 +1,8 @@
+const str = "Elzero";
+
+console.log(str.charAt(0).toLowerCase());
+console.log(str[0].toLowerCase());
+console.log(str.slice(0, 1).toLowerCase());
+console.log(str.substr(0, 1).toLowerCase());
+console.log(str.substring(0, 1).toLowerCase());
+console.log(str[0].toLowerCase().repeat(3));
