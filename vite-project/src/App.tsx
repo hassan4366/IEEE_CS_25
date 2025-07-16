e0e6d7490e2e47168095902db403e130ff1d0285
Hassan Hammam <hassanibrahimhammam@gmail.com>
2025-07-16 03:21:36 +0300

    edit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Stopwatch from "./stopwatch";
+import "./stopwatch.css";
+function App() {
+  return (
+    <>
+      <Stopwatch />
+    </>
+  );
+}
+
+export default App;
